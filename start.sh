004e6b836e2ca78283657c723c66f5656c77bf2c
Joey Whelan <joey.whelan@gmail.com>
2025-03-31 09:41:09 -0600

    fix start.sh

--- start.sh
+++ start.sh
@@ -5,13 +5,3 @@ source .env
 
 echo -e "\n*** ElasticSearch endpoint: https://$EAST_ELASTIC_IP:9200 ***"
 echo -e "*** Kibana endpoint: http://$EAST_KIBANA_IP:5601 ***"
-
-echo -e "\n*** Create east_ccs index ***"
-curl -s -k -u "elastic:elastic" "https://$EAST_ELASTIC_IP:9200/_bulk?pretty" \
-  -H "Content-Type: application/json" \
-  -d'
-{ "index" : { "_index" : "east_ccs" } }
-{"name": "Brave New World", "author": "Aldous Huxley", "release_date": "1932-06-01", "page_count": 268}
-{ "index" : { "_index" : "east_ccs" } }
-{"name": "The Handmaid'"'"'s Tale", "author": "Margaret Atwood", "release_date": "1985-06-01", "page_count": 311}
-' > /dev/null
